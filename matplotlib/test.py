47704fc09217ab715f58d67c92333c88bd301a96
HayataTakagi <h.takagi.work@gmail.com>
2021-01-08 14:14:00 +0900

    pnadast途中

--- matplotlib/test.py
+++ matplotlib/test.py
@@ -1,2 +1,17 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def main():
+    fig = plt.figure()
+    data = pd.read_csv('data/K-NE-SUM.csv')
+    df = data.iloc[0:10]
+    ax = fig.add_subplot(111, title="main", xlabel=df.index.name, ylabel='Processing time (ms)')
+    ax.plot(df['Point'])
+
+    fig.savefig('export/1-1_a.png', facecolor=fig.get_facecolor(), edgecolor=fig.get_edgecolor())
+
+
+if __name__ == '__main__':
+    main()
